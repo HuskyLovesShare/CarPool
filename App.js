27e995777c0133e329dbf1f92c8d032ee571e4f6
yijing-wu <yijingwu.w@gmail.com>
2023-02-04 11:04:14 -0800

    Add native-base for UI styling

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import * as React from "react";
-import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { NativeBaseProvider } from "native-base";
 
 import Tabs from "./components/Tabs";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Tabs />
-      </NavigationContainer>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Tabs />
+        </NavigationContainer>
+      </NativeBaseProvider>
     </SafeAreaProvider>
   );
 }
--- components/Home/ChooseIdentityScreen.js
+++ components/Home/ChooseIdentityScreen.js
@@ -1,25 +1,41 @@
 import React from "react";
-import { View, Button } from "react-native";
+import { View } from "react-native";
+
+import { NativeBaseProvider, Box, Button, Divider, Heading } from "native-base";
 
 export default function ChooseIdentityScreen({ navigation, route }) {
   console.log(route.params);
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        title="Share"
-        onPress={() => {
-          route.params.setIsDriver();
-          navigation.navigate("Direction", { isDriver: true });
-        }}
-      />
-      <Button
-        title="Find Share"
-        onPress={() => {
-          route.params.setIsRider();
-          navigation.navigate("Direction", { isDriver: false });
-        }}
-      />
+    <View style={{ flex: 1, alignItems: "center", marginTop: 150 }}>
+      <Box>
+        <Heading size="lg" fontSize={50} bold color={"lightBlue.500"}>
+          I want to{"     "}
+        </Heading>
+
+        <View style={{ height: 120 }} />
+        <Button
+          size="lg"
+          variant="outline"
+          onPress={() => {
+            route.params.setIsDriver();
+            navigation.navigate("Direction", { isDriver: true });
+          }}
+        >
+          Share
+        </Button>
+        <View style={{ height: 20 }} />
+        <Button
+          size="lg"
+          variant="outline"
+          onPress={() => {
+            route.params.setIsRider();
+            navigation.navigate("Direction", { isDriver: false });
+          }}
+        >
+          Find Share
+        </Button>
+      </Box>
     </View>
   );
 }
