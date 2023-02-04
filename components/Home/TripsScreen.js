de72b4e6fa63e2dd1b04a5bc418896cf4773bbf7
yijing-wu <yijingwu.w@gmail.com>
2023-02-04 12:25:06 -0800

    Add styles for ride information card

--- components/Home/TripsScreen.js
+++ components/Home/TripsScreen.js
@@ -1,57 +1,75 @@
 import React, { useState } from "react";
 import { ScrollView, View, Button, Text } from "react-native";
-import RideInformationCard from "./RideInformationCard";
+import RideCard from "../RideCard";
 
 export default function TripsScreen({ navigation, route }) {
   const [rides, setRides] = useState([
     {
+      date: "Feb 4",
       time: "8:15AM",
       location: "Bellevue Downtown",
       distance: 0.5,
-      fee: 20,
+      fee: 0,
       totalPositions: 3,
       available: 1,
+      driver: "Benjamin",
+      verifiedDriver: true,
     },
     {
-      time: "8:20AM",
+      date: "Feb 4",
+      time: "8:20 PM",
       location: "Bellevue Downtown",
       distance: 0.3,
       fee: 15,
       totalPositions: 3,
       available: 2,
+      driver: "John",
+      verifiedDriver: false,
     },
 
     {
-      time: "8:20AM",
+      date: "Feb 3",
+      time: "6:20 PM",
       location: "Bellevue Downtown",
       distance: 0.8,
-      fee: 15,
+      fee: 20,
       totalPositions: 3,
-      available: 2,
+      available: 3,
+      driver: "Bill",
+      verifiedDriver: true,
     },
     {
-      time: "8:20AM",
+      date: "Feb 3",
+      time: "12:20 PM",
       location: "Bellevue Downtown",
       distance: 1.0,
-      fee: 15,
+      fee: 0,
       totalPositions: 3,
-      available: 2,
+      available: 1,
+      driver: "Jack",
+      verifiedDriver: true,
     },
     {
-      time: "8:20AM",
+      date: "Feb 4",
+      time: "12:23 PM",
       location: "Bellevue Downtown",
       distance: 0.1,
-      fee: 15,
+      fee: 0,
       totalPositions: 3,
-      available: 2,
+      available: 1,
+      driver: "Julie",
+      verifiedDriver: false,
     },
     {
-      time: "8:20AM",
+      date: "Feb 5",
+      time: "7:20 AM",
       location: "Bellevue Downtown",
       distance: 0.7,
       fee: 15,
       totalPositions: 3,
       available: 2,
+      driver: "Rachel",
+      verifiedDriver: true,
     },
   ]);
 
@@ -62,7 +80,7 @@ export default function TripsScreen({ navigation, route }) {
         {rides
           .sort((a, b) => (a.distance > b.distance ? 1 : -1))
           .map((rideInfo, index) => (
-            <RideInformationCard
+            <RideCard
               key={index}
               index={index}
               rideInfo={rideInfo}
---
+++ components/RideCard.js
@@ -0,0 +1,136 @@
+import React from "react";
+
+import { View } from "react-native";
+
+import {
+  Text,
+  Pressable,
+  Box,
+  HStack,
+  Spacer,
+  Flex,
+  Badge,
+  Button,
+} from "native-base";
+
+export default function RideCard(props) {
+  const rideInfo = props.rideInfo;
+
+  return (
+    <Box alignItems="center" marginBottom={5}>
+      <Pressable width={"100%"} onPress={props.onPressed}>
+        {({ isHovered, isFocused, isPressed }) => {
+          return (
+            <Box
+              bg={
+                isPressed
+                  ? "coolGray.200"
+                  : isHovered
+                  ? "coolGray.200"
+                  : "coolGray.100"
+              }
+              style={{
+                transform: [
+                  {
+                    scale: isPressed ? 0.96 : 1,
+                  },
+                ],
+              }}
+              p="5"
+              rounded="8"
+              shadow={3}
+              borderWidth="1"
+              borderColor="coolGray.300"
+            >
+              <HStack alignItems="center">
+                {rideInfo.fee == 0 ? (
+                  <Badge
+                    colorScheme="green"
+                    _text={{
+                      color: "white",
+                    }}
+                    variant="solid"
+                    rounded="4"
+                  >
+                    Free
+                  </Badge>
+                ) : null}
+
+                {rideInfo.verifiedDriver ? (
+                  <>
+                    {rideInfo.fee == 0 ? <View style={{ width: 5 }} /> : null}
+                    <Badge
+                      colorScheme="lightBlue"
+                      _text={{
+                        color: "white",
+                      }}
+                      variant="solid"
+                      rounded="4"
+                    >
+                      Verified
+                    </Badge>
+                  </>
+                ) : null}
+
+                <Spacer />
+                <Text fontSize={10} color="coolGray.800">
+                  {`${rideInfo.distance} miles`}
+                </Text>
+              </HStack>
+              <Text
+                color="coolGray.800"
+                mt="1"
+                fontWeight="medium"
+                fontSize="lg"
+              >
+                {`${rideInfo.date} · ${rideInfo.time}`}
+              </Text>
+
+              <Text mt="0" fontSize="sm" color="coolGray.700">
+                {`Location: ${rideInfo.location}`}
+              </Text>
+
+              <Text mt="0" fontSize="sm" color="coolGray.700">
+                {`Fee: $${rideInfo.fee}`}
+              </Text>
+
+              <Text mt="0" fontSize="sm" color="coolGray.700">
+                {`Driver: ${rideInfo.driver}`}
+              </Text>
+
+              <Text mt="0" fontSize="sm" color="coolGray.700">
+                {`Available: ${rideInfo.available}/${rideInfo.totalPositions}`}
+              </Text>
+
+              <Flex>
+                {isFocused ? (
+                  <Button
+                    mt="2"
+                    fontSize={12}
+                    fontWeight="medium"
+                    textDecorationLine="underline"
+                    color="darkBlue.600"
+                    alignSelf="flex-start"
+                    onPress={props.onPressed}
+                  >
+                    Join
+                  </Button>
+                ) : (
+                  <Button
+                    mt="2"
+                    fontSize={12}
+                    fontWeight="medium"
+                    color="darkBlue.600"
+                    onPress={props.onPressed}
+                  >
+                    Join
+                  </Button>
+                )}
+              </Flex>
+            </Box>
+          );
+        }}
+      </Pressable>
+    </Box>
+  );
+}
