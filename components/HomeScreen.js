c8b6b0e11dc69a99f871e04b65733a1b7b87f484
Xiao Lan <xiaolan@Xiaos-MacBook-Air.local>
2023-02-04 09:58:35 -0800

    Adding Map

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,13 +1,73 @@
-import * as React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import RideInformationCard from "./RideInformationCard";
+import ChooseIdentityScreen from "./ChooseIdentityScreen";
+import ChooseDirectionScreen from "./ChooseDirectionScreen";
+import NewTripScreen from "./NewTripScreen";
+import ChooseMoreInfoScreen from "./ChooseMoreInfoScreen";
+import TripsScreen from "./TripsScreen";
+import TripDetailScreen from "./TripDetailScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-      <RideInformationCard />
-    </View>
-  );
+    const [isDriver, setIsDriver] = useState(true);
+    const [isToNEU, setIsToNEU] = useState(true);
+
+    return (
+        <Stack.Navigator
+            //   screenOptions={{
+            //     headerShown: false,
+            //   }}
+        >
+            <Stack.Screen
+                name="Home"
+                component={ChooseIdentityScreen}
+                initialParams={{
+                    isDriver: isDriver,
+                    setIsDriver: () => {
+                        setIsDriver(true);
+                    },
+                    setIsRider: () => {
+                        setIsDriver(false);
+                    },
+                }}
+            />
+            <Stack.Screen
+                name="Direction"
+                component={ChooseDirectionScreen}
+                initialParams={{
+                    isDriver: isDriver,
+                    setIsToNEU: setIsToNEU,
+                }}
+            />
+            <Stack.Screen
+                name="More Information"
+                component={ChooseMoreInfoScreen}
+                initialParams={{
+                    isDriver: isDriver,
+                    setIsToNEU: setIsToNEU,
+                    isToNEU: isToNEU,
+                }}
+            />
+            <Stack.Screen name="Trips" component={TripsScreen} />
+            <Stack.Screen
+                name="New Trip"
+                component={NewTripScreen}
+                initialParams={{
+                    isDriver: isDriver,
+                    setIsToNEU: setIsToNEU,
+                    isToNEU: isToNEU,
+                }}
+            />
+            <Stack.Screen
+                name="Trip Detail"
+                component={TripDetailScreen}
+                initialParams={{
+                    isDriver: isDriver,
+                    isToNEU: isToNEU,
+                }}
+            />
+        </Stack.Navigator>
+    );
 }
